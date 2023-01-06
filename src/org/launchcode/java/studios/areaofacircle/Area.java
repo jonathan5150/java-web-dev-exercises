25e094407d282d1659e3c256e239fd51c56d0ee6
Jonathan Rainey <jonathanmichael5150@gmail.com>
2023-01-05 19:53:44 -0600

    Finished studio, although def a few bugs left with user validation

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,31 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a radius: ");
+
+        Double userRadius = null;
+        while (userRadius == null) {
+            try {
+                userRadius = input.nextDouble();
+                if (userRadius < 0) {
+                    System.out.print("Input was invalid. Please enter a valid, positive number: ");
+                    input.nextDouble();
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("Input was invalid. Please enter a valid, positive number: ");
+                input.next();
+            }
+        }
+
+        input.close();
+        //double area = 3.14 * userRadius * userRadius;
+        //double area = Circle.getArea(userRadius);
+        System.out.println("The area of a circle of radius "
+                            + userRadius + " is: " + (Circle.getArea(userRadius)));
+    }
+}
